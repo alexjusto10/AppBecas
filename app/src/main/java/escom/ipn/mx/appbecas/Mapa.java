60a94b79abac8c115b14a11bf60423ef4e385e24
alexjusto10 <alexjusto10@hotmail.com>
2018-12-02 15:02:14 -0600

    -Se agregó mejor diseño al mapa, un spinner para las direcciones, un botón para regresar y un botón para generar el pdf.
    -Se agregó la activity Transporte.
    
    -Se agregó un Spinner a la pantalla principal el cuál manda los datos a Transporte.
    
    *Falta darle funcionalidad al botón del pdf, obtener las direcciones de la activity Transporte

--- app/src/main/java/escom/ipn/mx/appbecas/Mapa.java
+++ app/src/main/java/escom/ipn/mx/appbecas/Mapa.java
@@ -2,13 +2,16 @@ package escom.ipn.mx.appbecas;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-
+import android.view.View;
+import android.widget.Button;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,53 +30,83 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback,TaskLoa
     private GoogleMap mMap;
     private MarkerOptions lugarOrigen, escom;
     private Polyline currentPolyline;
-    private LocationManager locationManager;
+    private Button btn_route,btn_main,btn_pdf;
     private static final int LOCATION_REQUEST = 500;
-    ArrayList<LatLng> puntosLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapa);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)//Verifica si se tienen los permisos
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,//Se solicitan los permisos
                     Manifest.permission.ACCESS_COARSE_LOCATION},LOCATION_REQUEST);
         }
 
-        puntosLista = new ArrayList<>();
-
         LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);//Obtiene las coordenadas actuales
+        final double longitud = location.getLongitude();
+        final double latitud = location.getLatitude();
 
-        MapFragment fragmentoMapa = (MapFragment) getFragmentManager().findFragmentById(R.id.fragmentoMapa);
-        fragmentoMapa.getMapAsync(this);
+        btn_route = findViewById(R.id.btn_route);
+        btn_main = findViewById(R.id.btn_main);
 
-        double longitud = location.getLongitude();
-        double latitud = location.getLatitude();
+        Intent intent = getIntent();
+        int posicion = intent.getIntExtra("posicion",1);//Obtiene las coordenadas del activity Transporte
 
-        lugarOrigen = new MarkerOptions().position(new LatLng(latitud,longitud)).title("Casa");
+        lugarOrigen = new MarkerOptions().position(new LatLng(latitud,longitud)).title("Casa");//Se define el lugar del marcador
         escom = new MarkerOptions().position(new LatLng(19.5046539,-99.1468518)).title("ESCOM");
 
-        new FetchURL(Mapa.this).execute(getUrl(lugarOrigen.getPosition(), escom.getPosition(), "driving"), "driving");
-        //Zoom(mMap,latitud,longitud);
+        /*switch (posicion){
+            case 1:*/
+                btn_route.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new FetchURL(Mapa.this).execute(getUrl(lugarOrigen.getPosition()
+                                , escom.getPosition(), "driving"), "driving");//Une los datos y los manda en un Json
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(puntoMedio(lugarOrigen.getPosition(),escom.getPosition())
+                                ,13),1000,null);//Enfoca la cámara  13-distancia  1000-velocidad
+                    }
+                });
+
+                MapFragment fragmentoMapa = (MapFragment) getFragmentManager().findFragmentById(R.id.fragmentoMapa);
+                fragmentoMapa.getMapAsync(this);
+/*
+            break;
+
+            case 2:
+
+            break;
+        }*/
+
+        btn_main.setOnClickListener(new View.OnClickListener() {//Regresa a la pantalla principal
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {//Cuando el mapa ha cargado
         mMap = googleMap;
 
-        lugarOrigen.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        lugarOrigen.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));//Pone el marcador azul
         escom.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
-        mMap.addMarker(lugarOrigen);
-        mMap.addMarker(escom);
+        mMap.addMarker(lugarOrigen);//Pone el marcador en la casa
+        mMap.addMarker(escom);//Pone el marcador en ESCOM
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(puntoMedio(lugarOrigen.getPosition(),escom.getPosition())
+                ,13),5000,null);//Hace zoom en las coordenadas dadas.
+
     }
 
-    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {//Manda la petición de ruta
         // Origin of route
         String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
         // Destination of route
@@ -90,9 +123,20 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback,TaskLoa
     }
 
     @Override
-    public void onTaskDone(Object... values) {
+    public void onTaskDone(Object... values) {//Crea la ruta
         if (currentPolyline != null)
             currentPolyline.remove();
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
+    public LatLng puntoMedio (LatLng origen,LatLng destino){//Obtiene la distancia media entre los puntos para poder enfocar la cámara entre los dos
+
+        double lat = (origen.latitude + destino.latitude) / 2;
+
+        double lng = (origen.longitude + destino.longitude) /2;
+
+        LatLng resultado = new LatLng(lat,lng);
+
+        return  resultado;
     }
 }
